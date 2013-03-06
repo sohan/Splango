e9ca54f716fb9890e9a8bba33d3a1498f3b3708a
tsolar <tsolar@gmail.com>
2013-03-06 15:34:34 -0300

    splango.middleware: added docstrings

--- splango/middleware.py
+++ splango/middleware.py
@@ -4,10 +4,13 @@ from splango import RequestExperimentManager
 class ExperimentsMiddleware:
 
     def process_request(self, request):
+        """Assign the Experiment Manager to the request."""
         request.experiments_manager = RequestExperimentManager(request)
         return None
 
     def process_response(self, request, response):
+        """Retrieve the Experiment Manager from the request and assign it to
+        the response."""
         if getattr(request, "experiments_manager", None):
             request.experiments_manager.finish(response)
         return response

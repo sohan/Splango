423a6ffac33e40403469153f14d1b4afa4b0debc
tsolar <tsolar@gmail.com>
2013-03-13 11:33:18 -0300

    splango.admin: format fixes

--- splango/admin.py
+++ splango/admin.py
@@ -9,26 +9,36 @@ admin.site.register(Subject)
 
 class GoalAdmin(admin.ModelAdmin):
     list_display = ("name", "created")
+
+
 admin.site.register(Goal, GoalAdmin)
 
 
 class GoalRecordAdmin(admin.ModelAdmin):
     list_display = ("goal", "subject", "created", "req_HTTP_REFERER")
+
+
 admin.site.register(GoalRecord, GoalRecordAdmin)
 
 
 class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ("subject", "experiment", "variant", "created")
+
+
 admin.site.register(Enrollment, EnrollmentAdmin)
 
 
 class ExperimentAdmin(admin.ModelAdmin):
     list_display = ("name", "variants_commasep", "created")
+
+
 admin.site.register(Experiment, ExperimentAdmin)
 
 
 class ExperimentReportAdmin(admin.ModelAdmin):
     list_display = ("title", "experiment")
+
+
 admin.site.register(ExperimentReport, ExperimentReportAdmin)
 
 
@@ -36,4 +46,6 @@ class VariantAdmin(admin.ModelAdmin):
     list_display = ("name", "experiment",
                     # "weight"
     )
+
+
 admin.site.register(Variant, VariantAdmin)

3640dba917eede02d3d2069b7ebba0e242e35e31
tsolar <tsolar@gmail.com>
2013-03-15 15:58:07 -0300

    example.myapp.tests: added models factory for Experiment, Subject and Variant

--- example/myapp/tests.py
+++ example/myapp/tests.py
@@ -1,16 +1,31 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from splango.models import (Experiment, Variant, Subject)
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+def create_experiment(**kwargs):
+    defaults = {
+        'name': u'Experiment 1'
+    }
+    defaults.update(kwargs)
+    return Experiment.objects.create(**defaults)
+
+
+def create_subject(**kwargs):
+    defaults = {
+        #'user': None
+    }
+    defaults.update(kwargs)
+    return Subject.objects.create(**defaults)
+
+
+def create_variant(**kwargs):
+    defaults = {
+        'name': u'Variant 1'
+    }
+    defaults.update(kwargs)
+
+    if 'experiment' not in defaults:
+        defaults['experiment'] = create_experiment()
+
+    return Variant.objects.create(**defaults)
+

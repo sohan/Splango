6228bb7c7ed1ace734db85a08954aaaba6e2e04a
German Larrain <glarrain@example.com>
2013-03-22 12:57:57 -0400

    splango.admin: set 'list_filter' and 'date_hierarchy' for all models' admin classes

--- splango/admin.py
+++ splango/admin.py
@@ -17,6 +17,8 @@ admin.site.register(Subject, SubjectAdmin)
 
 class GoalAdmin(admin.ModelAdmin):
     list_display = ("name", "created")
+    list_filter = ('created',)
+    date_hierarchy = 'created'
 
 
 admin.site.register(Goal, GoalAdmin)
@@ -24,6 +26,8 @@ admin.site.register(Goal, GoalAdmin)
 
 class GoalRecordAdmin(admin.ModelAdmin):
     list_display = ("goal", "subject", "created", "req_HTTP_REFERER")
+    list_filter = ('goal', 'created')
+    date_hierarchy = 'created'
 
 
 admin.site.register(GoalRecord, GoalRecordAdmin)
@@ -31,6 +35,8 @@ admin.site.register(GoalRecord, GoalRecordAdmin)
 
 class EnrollmentAdmin(admin.ModelAdmin):
     list_display = ("subject", "experiment", "variant", "created")
+    list_filter = ('experiment', 'variant', 'created')
+    date_hierarchy = 'created'
 
 
 admin.site.register(Enrollment, EnrollmentAdmin)
@@ -38,6 +44,8 @@ admin.site.register(Enrollment, EnrollmentAdmin)
 
 class ExperimentAdmin(admin.ModelAdmin):
     list_display = ("name", "variants_commasep", "created")
+    list_filter = ('created',)
+    date_hierarchy = 'created'
 
 
 admin.site.register(Experiment, ExperimentAdmin)
@@ -45,6 +53,7 @@ admin.site.register(Experiment, ExperimentAdmin)
 
 class ExperimentReportAdmin(admin.ModelAdmin):
     list_display = ("title", "experiment")
+    list_filter = ('experiment',)
 
 
 admin.site.register(ExperimentReport, ExperimentReportAdmin)
@@ -54,6 +63,7 @@ class VariantAdmin(admin.ModelAdmin):
     list_display = ("name", "experiment",
                     # "weight"
     )
+    list_filter = ('experiment',)
 
 
 admin.site.register(Variant, VariantAdmin)

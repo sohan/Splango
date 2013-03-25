7cffaec98544915df07858bee0bd7d0e6ce6fb07
tsolar <tsolar@gmail.com>
2013-03-25 18:55:22 -0300

    example.myapp.urls: fixed indent format

--- example/myapp/urls.py
+++ example/myapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
-                       url(r'^sample/$', 'myapp.views.sample',
-                           name='myapp_sample'), )
+urlpatterns = patterns(
+    '',
+    url(r'^sample/$', 'myapp.views.sample', name='myapp_sample'),
+)

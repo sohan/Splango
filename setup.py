8ded7771995124675bd8e6da155f6d6e3fa59b65
German Larrain <glarrain@example.com>
2013-03-28 15:10:25 -0300

    setup: replaced 'distutils' with 'setuptools'; changed arguments of 'setup': * packages: from hardcoded to dynamic (via find_packages) * tests_require: added (['django>=1.3,<1.5',]) * test_suite: added ('runtests.runtests')

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+# Changed according to debug-toolbar to be able to run tests in a sane way,
+# with no external libraries (e.g. py.test, nose), and not using Django's
+# canonical way (which is fine for projects but not for apps to be distributed):
+# ./manage.py test
 
 setup(
     name='Splango',
@@ -9,6 +13,10 @@ setup(
     author='Shimon Rura',
     author_email='shimon@rura.org',
     url='http://github.com/shimon/Splango',
-    packages=['splango', 'splango.templatetags'],
-    package_data={'splango': ['templates/*.html', 'templates/*/*.html']}
+    packages=find_packages(exclude=('tests', 'example')),
+    package_data={'splango': ['templates/*.html', 'templates/*/*.html']},
+    tests_require=[
+        'django>=1.3,<1.5',
+    ],
+    test_suite='runtests.runtests',
 )
